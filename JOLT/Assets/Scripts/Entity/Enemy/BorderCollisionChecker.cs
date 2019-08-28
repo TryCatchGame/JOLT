cfeee95f0ac1c335afc8447a20d260b7dea44600
Kaynn-Cahya <kaynncahya@outlook.com>
2019-08-28 14:33:20 +0800

    Added Base Enemy Script
    An abstract enemy script for all enemies to inherit from

---
+++ JOLT/Assets/Scripts/Entity/Enemy/BorderCollisionChecker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace GameEntity.Enemy {
+    /// <summary>
+    /// Used to help to check if an enemy is touching the borders of the screen/camera.
+    /// </summary>
+    [RequireComponent(typeof(Enemy))]
+    public class BorderCollisionChecker : MonoBehaviour {
+
+        private Camera mainCameraRef;
+
+        private void Awake() {
+            mainCameraRef = Camera.main;
+        }
+
+        internal bool IsCollidingWithScreenBorder() {
+            Vector3 pos = mainCameraRef.WorldToViewportPoint(transform.position);
+
+            if (pos.x < 0.0) { return true; }
+            if (1.0 < pos.x) { return true; }
+            if (pos.y < 0.0) { return true; }
+            if (1.0 < pos.y) { return true; }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ JOLT/Assets/Scripts/Entity/Enemy/Enemy.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+using MyBox;
+
+using GameEntity.Player;
+
+namespace GameEntity.Enemy {
+    [RequireComponent(typeof(Collider2D), typeof(BorderCollisionChecker))]
+    public abstract class Enemy : MonoBehaviour {
+        protected enum CollisionType {
+            SCREEN_BORDER,
+            PLAYER
+        }
+
+        [Separator("Auto-Assign")]
+        [SerializeField, AutoProperty]
+        private BorderCollisionChecker borderCollisionChecker;
+
+        [Separator("Enemy Movement")]
+        [SerializeField, Tooltip("The move speed of the enemy"), PositiveValueOnly]
+        private float moveSpeed;
+
+        private bool isMovingToPlayerTarget;
+
+        private string playerTag;
+
+        protected Core PlayerTarget {
+            get; private set;
+        }
+
+        protected Vector2 PlayerTargetPosition {
+            get => PlayerTarget.transform.position;
+        }
+
+        /// <summary>
+        /// Which direction to move to if this enemy is not moving to the player target.
+        /// </summary>
+        protected Vector2 FreeMoveDirection {
+            get; private set;
+        }
+
+        private void Awake() {
+            isMovingToPlayerTarget = true;
+        }
+
+        void Update() {
+            if (isMovingToPlayerTarget) {
+                UpdateMovementToPlayerTarget();
+            } else {
+                UpdateMovementByFreeMoveDirection();
+
+                TargetPlayerIfOutOfBounds();
+            }
+
+            #region Local_Function
+
+            void UpdateMovementToPlayerTarget() {
+                transform.position = Vector2.MoveTowards(transform.position, PlayerTargetPosition, moveSpeed * Time.deltaTime);
+            }
+
+            void UpdateMovementByFreeMoveDirection() {
+                transform.position += (Vector3)FreeMoveDirection * moveSpeed * Time.deltaTime;
+            }
+
+            void TargetPlayerIfOutOfBounds() {
+                if (borderCollisionChecker.IsCollidingWithScreenBorder()) {
+                    isMovingToPlayerTarget = true;
+                    OnCollisionEvent(CollisionType.SCREEN_BORDER);
+
+                    CreateCollisionEffect();
+                }
+            }
+
+            #endregion
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision) {
+            if (collision.gameObject.CompareTag(playerTag)) {
+                OnCollisionEvent(CollisionType.PLAYER);
+
+                CreateCollisionEffect();
+                CreateDeathEffect();
+            }
+
+            #region Local_Function
+
+            void CreateDeathEffect() {
+                // TODO: Some visual effects on death.
+            }
+
+            #endregion
+        }
+
+        protected abstract void OnCollisionEvent(CollisionType collisionType);
+
+        #region Util
+
+        private void CreateCollisionEffect() {
+            // TODO: Some effects on *general* collision
+            // NOTE: This function should suit for all types of collision; No more, no less.
+        }
+
+        internal void SetPlayerTarget(Core target) {
+            PlayerTarget = target;
+            playerTag = PlayerTarget.tag;
+        }
+
+        protected void SetFreeMoveDirection(Vector2 moveDirection) {
+            FreeMoveDirection = moveDirection.normalized;
+        }
+
+        #endregion
+    }
+}
