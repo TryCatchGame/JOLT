826c6bd8d7fd97fea5fd813eb00aadef03317808
Kaynn-Cahya <kaynncahya@outlook.com>
2019-08-30 12:45:28 +0800

    Added FadableImage component
    Attach to Image to allow it to fade in/out using scripts

---
+++ JOLT/Assets/Scripts/Manager/TimeSlowManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using MyBox;
+
+namespace GameManager {
+
+    public class TimeSlowManager : Singleton<TimeSlowManager> {
+        [Separator("Display properties")]
+        [SerializeField, Tooltip("The background effect to show when time is slowed"), MustBeAssigned]
+        private Image timeSlowedImage;
+    }
+}
---
+++ JOLT/Assets/Scripts/Utils/FadableImage.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+using MyBox;
+
+namespace GameUtility {
+
+    [RequireComponent(typeof(Image))]
+    public class FadableImage : MonoBehaviour {
+
+        internal delegate void OnFadeCompleted();
+
+        [Separator("Auto Assign")]
+        [SerializeField, AutoProperty]
+        private Image imageToFade;
+
+        [Separator("Default Properties")]
+        [SerializeField, Tooltip("The default speed to fade at"), PositiveValueOnly]
+        private float defaultFadeSpeed;
+
+        private Coroutine fadeCoroutine;
+
+        private void Awake() {
+            if (imageToFade == null) {
+                imageToFade = GetComponent<Image>();
+            }
+        }
+
+        internal void FadeInImage(OnFadeCompleted callback) {
+            FadeInImage(callback, defaultFadeSpeed);
+        }
+
+        internal void FadeInImage(OnFadeCompleted callback, float fadeSpeed) {
+            StopFadeCoroutineIfExists();
+
+            fadeCoroutine = StartCoroutine(FadeCoroutine(0, 1, callback, fadeSpeed));
+        }
+
+        internal void FadeOutImage(OnFadeCompleted callback) {
+            FadeOutImage(callback, defaultFadeSpeed);
+        }
+
+        internal void FadeOutImage(OnFadeCompleted callback, float fadeSpeed) {
+            StopFadeCoroutineIfExists();
+
+            fadeCoroutine = StartCoroutine(FadeCoroutine(1, 0, callback, fadeSpeed));
+        }
+
+        #region Utils
+
+        internal IEnumerator FadeCoroutine(float fromAlpha, float toAlpha, OnFadeCompleted callback, float fadeSpeed) {
+            Color temp;
+
+            float progress = 0f;
+            while (progress < 1f) {
+                LerpImageAlphaColor(fromAlpha, toAlpha, progress);
+
+                yield return new WaitForEndOfFrame();
+                progress += (Time.unscaledDeltaTime * defaultFadeSpeed);
+            }
+
+            // Ensures that the image is completed faded at the end.
+            LerpImageAlphaColor(fromAlpha, toAlpha, 1);
+
+            yield return null;
+
+            #region Local_Function
+
+            void LerpImageAlphaColor(float a, float b, float t) {
+                temp = imageToFade.color;
+                temp.a = Mathf.Lerp(a, b, t);
+                imageToFade.color = temp;
+            }
+
+            #endregion
+        }
+
+        private void StopFadeCoroutineIfExists() {
+            if (fadeCoroutine != null) {
+                StopCoroutine(fadeCoroutine);
+            }
+        }
+
+        #endregion
+    }
+}
