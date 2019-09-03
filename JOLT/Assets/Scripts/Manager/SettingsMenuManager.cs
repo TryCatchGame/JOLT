660e8d1661f8e81b2581a238b4bbff961551af9c
Kaynn-Cahya <kaynncahya@outlook.com>
2019-09-03 18:32:58 +0800

    Basic setup of settings menu

---
+++ JOLT/Assets/Scripts/Manager/SettingsMenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyBox;
+namespace GameManager {
+    public class SettingsMenuManager : MonoBehaviour {
+        #region SettingsMenuButtons_Struct
+        [System.Serializable]
+        private struct SettingsMenuButton {
+        }
+        #endregion
+
+        #region SettingsMenuAnimator_Struct
+        [System.Serializable]
+        private struct SettingsMenuAnimator {
+            [SerializeField, Tooltip("The animator for the settings menu"), MustBeAssigned]
+            private Animator animController;
+
+            [SerializeField, Tooltip("The animation to show the settings menu"), MustBeAssigned]
+            private AnimationClip showSettingsMenuAnimation;
+
+            [SerializeField, Tooltip("The animation to hide the settings menu"), MustBeAssigned]
+            private AnimationClip hideSettingsMenuAnimation;
+
+            internal void PlayShowAnimation() {
+                animController.Play(showSettingsMenuAnimation.name);
+            }
+
+            internal void PlayHideAnimation() {
+                animController.Play(hideSettingsMenuAnimation.name);
+            }
+        }
+        #endregion
+
+        #region SettingsMenu_Struct
+        [System.Serializable]
+        private struct SettingsMenu {
+            [SerializeField, Tooltip("The respective animation for the settings menu"), MustBeAssigned]
+            private SettingsMenuAnimator animator;
+
+            internal void PlayHideSettingsAnimation() {
+                animator.PlayHideAnimation();
+            }
+
+            internal void PlayShowSettingsAnimation() {
+                animator.PlayShowAnimation();
+            }
+        }
+        #endregion
+    }
+}
