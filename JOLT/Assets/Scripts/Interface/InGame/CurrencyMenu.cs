bd90bbe7917a756555efc26ea89bd66e665a4aea
Kaynn-Cahya <kaynncahya@outlook.com>
2019-09-04 12:47:49 +0800

    Added animation effect when gem is added

--- JOLT/Assets/Scripts/Interface/InGame/CurrencyMenu.cs
+++ JOLT/Assets/Scripts/Interface/InGame/CurrencyMenu.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using TMPro;
 using MyBox;
-
 namespace GameInterface.InGame {
     public class CurrencyMenu : MonoBehaviour {
+        // NOTE: Possible refactor into a 'CurrencyText' struct?
         [Separator("Visual displays")]
         [SerializeField, Tooltip("The currency text for the in game menu"), MustBeAssigned]
         private TextMeshProUGUI currencyText;
+
+        [SerializeField, Tooltip("The animator for the currency text"), MustBeAssigned]
+        private Animator currencyTextAnimator;
+
+        [SerializeField, Tooltip("The animation to play when the currency is changed"), MustBeAssigned]
+        private AnimationClip currencyChangedAnimation;
 
         void Awake() {
             currencyText.text = GlobalProperties.TotalGemCount_Local.ToString();
@@ -14,6 +20,8 @@ namespace GameInterface.InGame {
 
         internal void UpdateGemCountText(int gem) {
             currencyText.text = gem.ToString();
+
+            currencyTextAnimator.Play(currencyChangedAnimation.name, -1, 0f);
         }
     }
 }
