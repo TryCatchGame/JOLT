abba1a021e8bb8528d6766e4f3502088d4306ba7
tan jun rong <tanjunrong321@gmail.com>
2019-09-06 22:55:06 +0800

    Updated enemy trail + added camera shake

--- JOLT/Assets/Scripts/Entity/Enemy/Enemy.cs
+++ JOLT/Assets/Scripts/Entity/Enemy/Enemy.cs
@@ -6,144 +6,145 @@ using GameEntity.Player;
 using GameParticle.Enemy;
 
 namespace GameEntity.Enemy {
-    [RequireTag("Enemy")]
-    [RequireLayer("Enemy")]
-    [RequireComponent(typeof(Collider2D), typeof(BorderCollisionChecker))]
-    public abstract class Enemy : MonoBehaviour {
-        protected enum CollisionType {
-            SCREEN_BORDER,
-            PLAYER
-        }
+	[RequireTag("Enemy")]
+	[RequireLayer("Enemy")]
+	[RequireComponent(typeof(Collider2D), typeof(BorderCollisionChecker))]
+	public abstract class Enemy : MonoBehaviour {
+		protected enum CollisionType {
+			SCREEN_BORDER,
+			PLAYER
+		}
 
-        [Separator("Auto-Assign")]
-        [SerializeField, AutoProperty]
-        private BorderCollisionChecker borderCollisionChecker;
+		[Separator("Auto-Assign")]
+		[SerializeField, AutoProperty]
+		private BorderCollisionChecker borderCollisionChecker;
 
-        [Separator("Enemy Movement")]
-        [SerializeField, Tooltip("The move speed of the enemy"), PositiveValueOnly]
-        private float moveSpeed;
+		[Separator("Enemy Movement")]
+		[SerializeField, Tooltip("The move speed of the enemy"), PositiveValueOnly]
+		private float moveSpeed;
 
-        [Separator("Wall Bounce properties")]
-        [SerializeField, Tooltip("True if this enemy creates an effect when bouncing off the walls")]
-        private bool effectOnWallBounce;
+		[Separator("Wall Bounce properties")]
+		[SerializeField, Tooltip("True if this enemy creates an effect when bouncing off the walls")]
+		private bool effectOnWallBounce;
 
-        [SerializeField, Tooltip("The effect it creates"), ConditionalField(nameof(effectOnWallBounce), false, true), MustBeAssigned]
-        private EnemyBounceEffect wallBounceEffect;
+		[SerializeField, Tooltip("The effect it creates"), ConditionalField(nameof(effectOnWallBounce), false, true), MustBeAssigned]
+		private EnemyBounceEffect wallBounceEffect;
 
-        private string playerTag;
+		private string playerTag;
 
-        protected Core PlayerTarget {
-            get; private set;
-        }
+		protected Core PlayerTarget {
+			get; private set;
+		}
 
-        protected Vector2 PlayerTargetPosition {
-            get => PlayerTarget.transform.position;
-        }
+		protected Vector2 PlayerTargetPosition {
+			get => PlayerTarget.transform.position;
+		}
 
-        /// <summary>
-        /// Which direction to move to if this enemy is not moving to the player target.
-        /// </summary>
-        protected Vector2 FreeMoveDirection {
-            get; private set;
-        }
+		/// <summary>
+		/// Which direction to move to if this enemy is not moving to the player target.
+		/// </summary>
+		protected Vector2 FreeMoveDirection {
+			get; private set;
+		}
 
-        protected bool IsMovingToPlayerTarget {
-            get; private set;
-        }
+		protected bool IsMovingToPlayerTarget {
+			get; private set;
+		}
 
-        protected float Radius {
-            get => borderCollisionChecker.Radius; 
-        }
+		protected float Radius {
+			get => borderCollisionChecker.Radius;
+		}
 
-        private void Awake() {
-            if (borderCollisionChecker == null) {
-                borderCollisionChecker = GetComponent<BorderCollisionChecker>();
-            }
+		private void Awake() {
+			if(borderCollisionChecker == null) {
+				borderCollisionChecker = GetComponent<BorderCollisionChecker>();
+			}
 
-            IsMovingToPlayerTarget = true;
-        }
+			IsMovingToPlayerTarget = true;
+		}
 
-        void Update() {
-            if (IsMovingToPlayerTarget) {
-                UpdateMovementToPlayerTarget();
-            } else {
-                UpdateMovementByFreeMoveDirection();
+		void Update() {
+			if(IsMovingToPlayerTarget) {
+				UpdateMovementToPlayerTarget();
+			} else {
+				UpdateMovementByFreeMoveDirection();
 
-                TargetPlayerIfOutOfBounds();
-            }
+				TargetPlayerIfOutOfBounds();
+			}
 
-            #region Local_Function
+			#region Local_Function
 
-            void UpdateMovementToPlayerTarget() {
-                transform.position = Vector2.MoveTowards(transform.position, PlayerTargetPosition, moveSpeed * Time.deltaTime);
-            }
+			void UpdateMovementToPlayerTarget() {
+				transform.position = Vector2.MoveTowards(transform.position, PlayerTargetPosition, moveSpeed * Time.deltaTime);
+			}
 
-            void UpdateMovementByFreeMoveDirection() {
-                transform.position += (Vector3)FreeMoveDirection * moveSpeed * Time.deltaTime;
-            }
+			void UpdateMovementByFreeMoveDirection() {
+				transform.position += (Vector3)FreeMoveDirection * moveSpeed * Time.deltaTime;
+			}
 
-            void TargetPlayerIfOutOfBounds() {
-                if (borderCollisionChecker.IsCollidingWithScreenBorder()) {
-                    IsMovingToPlayerTarget = true;
-                    OnCollisionEvent(CollisionType.SCREEN_BORDER);
+			void TargetPlayerIfOutOfBounds() {
+				if(borderCollisionChecker.IsCollidingWithScreenBorder()) {
+					IsMovingToPlayerTarget = true;
+					OnCollisionEvent(CollisionType.SCREEN_BORDER);
 
-                    CreateWallCollisionEffect();
-                }
-            }
+					CreateWallCollisionEffect();
+				}
+			}
 
-            void CreateWallCollisionEffect() {
-                if (effectOnWallBounce) {
-                    var newWallBounceEffect = Instantiate(wallBounceEffect);
-                    newWallBounceEffect.transform.position = transform.position;
-                }
-            }
+			void CreateWallCollisionEffect() {
+				if(effectOnWallBounce) {
+					var newWallBounceEffect = Instantiate(wallBounceEffect);
+					newWallBounceEffect.transform.position = transform.position;
+				}
+			}
 
-            #endregion
-        }
+			#endregion
+		}
 
-        private void OnCollisionEnter2D(Collision2D collision) {
-            if (collision.gameObject.CompareTag(playerTag)) {
-                OnCollisionEvent(CollisionType.PLAYER);
+		private void OnCollisionEnter2D(Collision2D collision) {
+			if(collision.gameObject.CompareTag(playerTag)) {
+				OnCollisionEvent(CollisionType.PLAYER);
 
-                TriggerCollisionDeath();
-            }
+				TriggerCollisionDeath();
+				CameraShake.Instance.TriggerShake();
+			}
 
-            #region Local_Function
+			#region Local_Function
 
-            void TriggerCollisionDeath() {
-                CreateDeathEffect();
+			void TriggerCollisionDeath() {
+				CreateDeathEffect();
 
-                Destroy(gameObject);
-            }
+				Destroy(gameObject);
+			}
 
-            void CreateDeathEffect() {
-                // TODO: Some visual effects on death.
-            }
+			void CreateDeathEffect() {
+				// TODO: Some visual effects on death.
+			}
 
-            #endregion
-        }
+			#endregion
+		}
 
-        protected abstract void OnCollisionEvent(CollisionType collisionType);
+		protected abstract void OnCollisionEvent(CollisionType collisionType);
 
-        #region Util
+		#region Util
 
-        internal void SetPlayerTarget(Core target) {
-            PlayerTarget = target;
-            playerTag = PlayerTarget.tag;
-        }
+		internal void SetPlayerTarget(Core target) {
+			PlayerTarget = target;
+			playerTag = PlayerTarget.tag;
+		}
 
-        internal void SetFreeMoveDirection(Vector2 moveDirection, bool enableFreeMove = true) {
-            FreeMoveDirection = moveDirection.normalized;
+		internal void SetFreeMoveDirection(Vector2 moveDirection, bool enableFreeMove = true) {
+			FreeMoveDirection = moveDirection.normalized;
 
-            if (enableFreeMove) {
-                IsMovingToPlayerTarget = false;
-            }
-        }
+			if(enableFreeMove) {
+				IsMovingToPlayerTarget = false;
+			}
+		}
 
-        protected Vector2 GetDirectionAwayFromPlayer() {
-            return (PlayerTargetPosition - (Vector2)transform.position).normalized * -1f;
-        }
+		protected Vector2 GetDirectionAwayFromPlayer() {
+			return (PlayerTargetPosition - (Vector2)transform.position).normalized * -1f;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
---
+++ JOLT/Assets/Scripts/Utils/CameraShake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class CameraShake : Singleton<CameraShake> {
+	[Header("Shake properties")]
+	[SerializeField, Tooltip("How long should the shake last for?")] private float shakeDuration;
+	[SerializeField, Tooltip("How strong is the shake?")] private float shakeAmount;
+
+	private Vector3 currentPos;
+
+	public void TriggerShake() {
+		StartCoroutine(Shake());
+	}
+
+	private IEnumerator Shake() {
+		// Initialize a timer.
+		float currentShakeDuration = shakeDuration;
+
+		// Save the position before shaking.
+		currentPos = transform.localPosition;
+
+		while(currentShakeDuration > 0) {
+			// Randomly move the camera around.
+			transform.localPosition = currentPos + Random.insideUnitSphere * shakeAmount;
+
+			// Start counting down before stopping the shake.
+			currentShakeDuration -= Time.unscaledDeltaTime;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		// Move the camera back to its previous position.
+		transform.localPosition = currentPos;
+	}
+}
