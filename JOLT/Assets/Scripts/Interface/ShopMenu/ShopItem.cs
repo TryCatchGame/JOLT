1f6b77badac664cd6f9a44a8137dd868667304a2
Kaynn Cahya <kaynncahya@outlook.com>
2019-09-10 21:18:55 +0900

    Made shop menu able to dynamically load shop items based on inspector values.

---
+++ JOLT/Assets/Scripts/Interface/ShopMenu/ShopItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using MyBox;
+namespace GameInterface.ShopMenu {
+    public class ShopItem : MonoBehaviour {
+        [SerializeField, Tooltip("The image to show the content of this item"), MustBeAssigned]
+        private Image contentImage;
+
+        [SerializeField, Tooltip("The text to show the price of this item"), MustBeAssigned]
+        private TextMeshProUGUI contentPriceText;
+
+        internal void SetShopItemContent(Sprite contentSprite, int contentPrice) {
+            contentImage.sprite = contentSprite;
+            contentPriceText.text = contentPrice.ToString();
+        }
+    }
+
+}
---
+++ JOLT/Assets/Scripts/Interface/ShopMenu/ShopItemsMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+using MyBox;
+namespace GameInterface.ShopMenu {
+    /// <summary>
+    /// Attach it to the game-object that is responsible for having all the shop items as it's child elements.
+    /// </summary>
+    public class ShopItemsMenu : MonoBehaviour {
+        #region PurchasableCore
+        [System.Serializable]
+        private class PurchasableCore {
+            [SerializeField, Tooltip("The sprite of the core"), MustBeAssigned]
+            private Sprite coreSprite;
+
+            [SerializeField, Tooltip("The price of the core"), PositiveValueOnly]
+            private int corePrice = 100;
+
+            public int CorePrice { get => corePrice; }
+            public Sprite CoreSprite { get => coreSprite; }
+            // TODO: Some other identifier for the core's skin?
+            public string CoreName { get => coreSprite.name; }
+        }
+        #endregion
+
+        [SerializeField, Tooltip("Prefab of a shop item"), MustBeAssigned]
+        private ShopItem shopItemPrefab;
+
+        [Separator()]
+        [SerializeField, Tooltip("The list of all the cores that can be purchased in the shop"), MustBeAssigned]
+        private PurchasableCore[] purchasableCores;
+
+        private void Awake() {
+            PopulateShopItemsMenuWithPurchasableCores();
+
+            // This component is not needed anymore.
+            Destroy(this);
+            #region Local_Function
+            void PopulateShopItemsMenuWithPurchasableCores() {
+                foreach (var purchasableCore in purchasableCores) {
+                    var newShopItem = Instantiate(shopItemPrefab);
+                    newShopItem.transform.SetParent(transform, false);
+
+                    newShopItem.SetShopItemContent(purchasableCore.CoreSprite, purchasableCore.CorePrice);
+                }
+            }
+            #endregion
+        }
+    }
+}
